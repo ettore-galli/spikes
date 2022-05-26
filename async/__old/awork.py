837e0f35a54299546d928fc371bee9b612c5805d
Ettore Galli <ettore.galli@gmail.com>
2022-05-26 23:07:59 +0200

    async experiments

---
+++ async/__old/awork.py
@@ -0,0 +1,18 @@
+import asyncio
+
+
+async def print_delayed(message: str, delay: int = 1):
+    await asyncio.sleep(delay)
+    print(message)
+    return f"Result of {message}"
+
+
+async def amain():
+    tasks = [asyncio.create_task(print_delayed(f"task-{i}", 4)) for i in range(4)]
+    print(
+        await asyncio.gather(*tasks, return_exceptions=True)
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(amain())
