48dcfb8a258036e001706a29ab667218dd848a34
Ettore Galli <ettore.galli@gmail.com>
2022-08-03 07:14:35 +0200

    executor

---
+++ filosofia_calcoli/m1.py
@@ -0,0 +1,7 @@
+from mutils import funzione
+
+s = inputs["x"] + inputs["y"]
+p = inputs["x"] * inputs["y"]
+
+
+result = funzione(s, p)
---
+++ filosofia_calcoli/mutils.py
@@ -0,0 +1,2 @@
+def funzione(a, b):
+    return a**2 + b**2 + 1
