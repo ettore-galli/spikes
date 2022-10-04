7e4690f73eec0efd634846ff8863cda3b7fad055
Ettore Galli <ettore.galli@deustechnology.com>
2022-10-04 21:55:29 +0200

    BAsic rpn calc

--- react/smart-fe/src/calc/calc.tsx
+++ react/smart-fe/src/calc/calc.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-import { doCalcEnter, doCalcDrop, doSetInputItem } from './calc_reducer';
+import { doCalcEnter, doCalcDrop, doSetInputItem, doCalcSum, doCalcSub, doCalcMul, doCalcDiv } from './calc_reducer';
 
 interface Props {
     state: any,
     dispatch: any
 }
-
-
 
 function Calc(props: Props) {
 
@@ -23,17 +21,38 @@ function Calc(props: Props) {
         props.dispatch(doCalcDrop())
     }
 
+    const stackSum = () => {
+        props.dispatch(doCalcSum())
+    }
+
+    const stackSub = () => {
+        props.dispatch(doCalcSub())
+    }
+
+    const stackMul = () => {
+        props.dispatch(doCalcMul())
+    }
+
+    const stackDiv = () => {
+        props.dispatch(doCalcDiv())
+    }
+
     return (
-        <>  {props.state.stack}
-            {props.state.inputItem}
+        <>
             {
                 props.state.stack.map((item: any, index: number) => {
                     return <input key={index} value={item} readOnly={true} />
                 })
             }
+            <hr />
             <input key={"new"} value={props.state.inputItem} onChange={setNewValue} />
+            <hr />
             <input type="button" value="ENTER" onClick={enter} />
             <input type="button" value="DROP" onClick={drop} />
+            <input type="button" value="[+]" onClick={stackSum} />
+            <input type="button" value="[-]" onClick={stackSub} />
+            <input type="button" value="[*]" onClick={stackMul} />
+            <input type="button" value="[/]" onClick={stackDiv} />
         </>
 
     );
--- react/smart-fe/src/calc/calc_reducer.tsx
+++ react/smart-fe/src/calc/calc_reducer.tsx
@@ -1,5 +1,22 @@
 export const calcReducerInitialState = { stack: [1, 2, 3], inputItem: "" };
 
+type StackFunction = ((a: any, b: any) => any) | ((a: any) => any);
+const performOnStack = (stack: any[], performer: StackFunction): any[] => {
+
+    if (stack.length > 1) {
+        try {
+            const sliced = stack.slice(-2);
+            const [a, b] = sliced;
+            return [...sliced, performer(a, b)]
+        } catch (e) {
+            return [...stack, String(e)]
+        }
+
+    }
+
+    return stack;
+}
+
 export function calcReducer(state: any, action: { type: string, payload: any }) {
     switch (action.type) {
         case 'enter':
@@ -8,6 +25,14 @@ export function calcReducer(state: any, action: { type: string, payload: any }) 
             return { ...state, stack: [...state.stack.slice(0, -1)] };
         case 'set-input-item':
             return { ...state, inputItem: action.payload.item };
+        case 'sum':
+            return { ...state, stack: performOnStack(state.stack, (a, b) => a + b) };
+        case 'sub':
+            return { ...state, stack: performOnStack(state.stack, (a, b) => a - b) };
+        case 'mul':
+            return { ...state, stack: performOnStack(state.stack, (a, b) => a * b) };
+        case 'div':
+            return { ...state, stack: performOnStack(state.stack, (a, b) => a / b) };
         default:
             throw new Error();
     }
@@ -23,4 +48,20 @@ export function doCalcEnter(): { type: string } {
 
 export function doCalcDrop(): { type: string } {
     return { type: "drop" };
+}
+
+export function doCalcSum(): { type: string } {
+    return { type: "sum" };
+}
+
+export function doCalcSub(): { type: string } {
+    return { type: "sub" };
+}
+
+export function doCalcMul(): { type: string } {
+    return { type: "mul" };
+}
+
+export function doCalcDiv(): { type: string } {
+    return { type: "div" };
 }
