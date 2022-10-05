385f00313a243efa525ac7dd956c66593661d5c6
Ettore Galli <ettore.galli@deustechnology.com>
2022-10-05 21:55:50 +0200

    bindActionCreators

---
+++ react/smart-fe/src/calc/reducer-utils/reducer-utils.ts
@@ -0,0 +1,12 @@
+
+
+export const bindActionCreators = (actionCreators: { [key: string]: ((...args: any) => Object) }, dispatch: ((action: any) => void)) => {
+    return Object.keys(actionCreators).reduce((prv, cur) => {
+        return {
+            ...prv, [cur]: ((...args: any) => {
+                return dispatch(actionCreators[cur](...args));
+            })
+        }
+    }, {});
+}
+
