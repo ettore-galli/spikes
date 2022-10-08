f2356ca10ecadc255c60af1c5548b190d713122e
Ettore Galli <ettore.galli@deustechnology.com>
2022-10-08 10:01:17 +0200

    Two different examples

--- react/exmple-fe-1/src/App.tsx
+++ react/exmple-fe-1/src/App.tsx
@@ -2,12 +2,24 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Example1Page from './components/example-1-page/example-1-page';
+import Example2Page from './components/example-2-page/example-2-page';
 
 function App() {
   return (
     <div className='App'>
       <Example1Page />
+
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" >
+          <Route path="example-1" element={<Example1Page />} />
+          <Route path="example-2" element={<Example2Page />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </div>
   )
 }
