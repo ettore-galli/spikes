811351a28dd9498e6581fd8fb7d8f9fd2c9f0700
Ettore Galli <ettoregalli@MacBook-Pro-di-Ettore-2023.local>
2023-04-21 20:30:49 +0200

    join any objects

--- join_lists/test_join.py
+++ join_lists/test_join.py
@@ -1,4 +1,5 @@
-from typing import Any, Callable, Iterable
+from dataclasses import dataclass
+from typing import Any, Callable, Iterable, Optional
 
 from join_lists.join import join
 
@@ -25,5 +26,60 @@ def test_join():
 
     assert (
         join(finite_iterable_a, finite_iterable_b, join_key_a, join_key_b)
+        == expected_join
+    )
+
+
+def test_join_classes():
+    @dataclass
+    class AClass:
+        a: int
+        x: str
+
+    @dataclass
+    class BClass:
+        a: int
+        z: str
+
+    @dataclass
+    class JoinedClass:
+        a: int
+        x: Optional[str]
+        z: Optional[str]
+
+    finite_iterable_a: Iterable[Any] = [
+        AClass(1, "aaa"),
+        AClass(2, "bbb"),
+        AClass(3, "ccc"),
+    ]
+    finite_iterable_b: Iterable[Any] = [
+        BClass(1, "qqqqqq"),
+        BClass(3, "wwwwww"),
+        BClass(4, "eeeeee"),
+    ]
+    join_key_a: Callable[[Any], Any] = lambda item: item.a
+    join_key_b: Callable[[Any], Any] = lambda item: item.a
+
+    def combineAB(item_a: AClass, item_b: Optional[BClass]) -> JoinedClass:
+        return (
+            JoinedClass(item_a.a, item_a.x, item_b.z)
+            if item_b
+            else JoinedClass(item_a.a, item_a.x, None)
+        )
+
+    expected_join: Iterable[Any] = [
+        JoinedClass(1, "aaa", "qqqqqq"),
+        JoinedClass(2, "bbb", None),
+        JoinedClass(3, "ccc", "wwwwww"),
+    ]
+
+    assert (
+        join(
+            finite_iterable_a,
+            finite_iterable_b,
+            join_key_a,
+            join_key_b,
+            combiner=combineAB,
+        )
         == expected_join
     )
