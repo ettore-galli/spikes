cea920b2fd1c44cb074534686231e8eed8ced0ca
ettore-galli <ettore.galli@gmail.com>
2023-08-24 10:00:04 +0200

    sqla basic

---
+++ sqlalchemy/connection.py
@@ -0,0 +1,34 @@
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class DBSettings:
+    url = "sqlite:///./db/database.db"
+
+
+def create_db_engine(db_settings: DBSettings) -> sqlalchemy.engine.base.Engine:
+    return create_engine(db_settings.url)
+
+
+def create_tables(engine: sqlalchemy.engine.base.Engine):
+    Base.metadata.create_all(engine)
+
+
+def initialize_database():
+    create_tables(create_db_engine(DBSettings()))
+
+
+def build_db_session(engine: sqlalchemy.engine.base.Engine):
+    Session = sessionmaker(engine)
+    session = Session()
+    print(type(session))
+    return session
+
+
+def create_db_session():
+    return build_db_session(create_db_engine(DBSettings()))
---
+++ sqlalchemy/sqa_init.py
@@ -0,0 +1,9 @@
+from connection import initialize_database
+from models import Base
+
+def main():
+    initialize_database()
+
+
+if __name__ == "__main__":
+    main()
