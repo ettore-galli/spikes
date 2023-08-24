1da96becc48e7ed4e9b8fbb5a82358878acd935f
ettore-galli <ettore.galli@gmail.com>
2023-08-24 11:13:09 +0200

    two tables

--- sqlalchemy/models.py
+++ sqlalchemy/models.py
@@ -1,6 +1,16 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 from connection import Base
+
+
+class Department(Base):
+    __tablename__ = "departments"
+
+    id = Column(Integer, primary_key=True)
+    descr = Column(String)
+
+    employees = relationship("Employee", back_populates="department")
 
 
 class Employee(Base):
@@ -8,3 +18,6 @@ class Employee(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
+    department_id = Column(Integer, ForeignKey("departments.id"))
+    
+    department = relationship("Department", back_populates="employees")
--- sqlalchemy/sqa_work.py
+++ sqlalchemy/sqa_work.py
@@ -1,16 +1,23 @@
 from connection import create_db_session
-from models import Employee
+from models import Employee, Department
 
 
 def insert_data(session):
+    for dep in ["RD", "Mkt"]:
+        session.add(Department(descr=dep))
+    session.commit()
+
     for name in ["Ettore", "aaa", "bbb", "ccc"]:
-        session.add(Employee(name=name))
+        session.add(Employee(name=name, department_id=(1 if name == "Ettore" else 2)))
     session.commit()
 
 
 def query_data(session):
-    for record in session.query(Employee).all():
-        print(record.__dict__)
+    for dep in session.query(Department).all():
+        print(f"========== {dep.id}: {dep.descr} ==========")
+
+        for emp in dep.employees:
+            print(f"* {emp.id}: {emp.name} ")
 
 
 def main():
