5c8547995bd21a7baec84b8310dff1c3c99667f8
Ettore Galli <ettore.galli@eng.it>
2023-11-08 22:46:13 +0100

    IMprove demo

--- patterns/corso-eng/DEV-MEETING/examples/chain.py
+++ patterns/corso-eng/DEV-MEETING/examples/chain.py
@@ -7,6 +7,10 @@ Number = Union[int, float]
 
 
 class PowerHandler(ABC):
+    """
+    "Handler"
+    """
+
     def __init__(self, next: Optional[PowerHandler] = None) -> None:
         super().__init__()
         self.set_next(next)
@@ -20,35 +24,50 @@ class PowerHandler(ABC):
 
 
 class RepeatedMultiplicationsHandler(PowerHandler):
+    """
+    "Concrete Handler"
+    """
+
     def compute_power(self, base: Number, exponent: Number) -> Number:
         if isinstance(exponent, int) and exponent < 10:
             power: float = 1
             for _ in range(exponent):
                 power = power * base
-            print(f"Handled by {self.__class__.__name__}")
+            print(f"\n{base} ^ {exponent}: ==> Handled by {self.__class__.__name__}")
             return power
 
         return self.next.compute_power(base, exponent)
 
 
 class MathPowHandler(PowerHandler):
+    """
+    "Concrete Handler"
+    """
+
     def compute_power(self, base: Number, exponent: Number) -> Number:
         if base < 1000000 and exponent < 50:
-            print(f"Handled by {self.__class__.__name__}")
+            print(f"\n{base} ^ {exponent}: ==> Handled by {self.__class__.__name__}")
             return math.pow(base, exponent)
 
         return self.next.compute_power(base, exponent)
 
 
 class BigNumbersHandler(PowerHandler):
+    """
+    "Concrete Handler"
+    """
+
     def compute_power(self, base: Number, exponent: Number) -> Number:
-        print(f"Handled by {self.__class__.__name__}")
+        print(f"\n{base} ^ {exponent}: ==> Handled by {self.__class__.__name__}")
         lower = int(base) ** int(exponent)
         upper = int(base + 1) ** int(exponent + 1)
         return (lower + upper) // 2
 
 
 def power(base: Number, exponent: Number) -> Number:
+    """
+    "Client"
+    """
     handler_1 = RepeatedMultiplicationsHandler()
     handler_2 = MathPowHandler()
     handler_3 = BigNumbersHandler()
@@ -58,6 +77,6 @@ def power(base: Number, exponent: Number) -> Number:
 
 
 if __name__ == "__main__":
-    print(power(2, 3))
-    print(power(11, 12))
-    print(power(111111.4, 51.8))
+    print("=", power(2, 3))
+    print("=", power(11, 12))
+    print("=", power(111111.4, 51.8))
