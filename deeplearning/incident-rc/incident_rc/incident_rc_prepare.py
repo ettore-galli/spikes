90109fefece83f5afd7e661258dae4f6282f37f0
ettore-galli <ettore.galli@gmail.com>
2023-11-21 09:06:51 +0100

    do not rescale

--- deeplearning/incident-rc/incident_rc/incident_rc_prepare.py
+++ deeplearning/incident-rc/incident_rc/incident_rc_prepare.py
@@ -26,13 +26,33 @@ def prepare_row(
 def load_incident_rc_data(incident_rc_data_file: str) -> np.ndarray:
     with open(incident_rc_data_file, newline="", encoding="utf-8") as csvfile:
         reader = csv.DictReader(csvfile)
-        return np.array([prepare_row(list(row.values())) for row in reader])
+        prepared_rows = np.array(
+            [prepare_row(list(row.values())) for row in reader], dtype="float"
+        )
+        print(prepared_rows)
+
+        return prepared_rows
 
 
 def split_features_target(data: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     features = data[:, 1:-1]
     target = data[:, -1]
     return features, target
+
+
+def load_incident_rc_data_splitted(
+    incident_rc_data_file: str,
+) -> Tuple[np.ndarray, np.ndarray]:
+    with open(incident_rc_data_file, newline="", encoding="utf-8") as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        raw_data = [tuple(row.values()) for row in reader]
+
+        features = np.array(
+            [tuple(float_or_none(item) for item in row[1:-1]) for row in raw_data]
+        )
+        target = np.array([str(row[-1]) for row in raw_data])
+        return features, target
 
 
 def categorize_target(data: np.ndarray) -> Tuple[np.ndarray, LabelEncoder]:
@@ -63,10 +83,12 @@ def rescale_input(data: np.ndarray) -> np.ndarray:
 
 
 def prepare_incident_rc_data(incident_rc_data_file: str):
-    data = load_incident_rc_data(incident_rc_data_file)
-    features, target = split_features_target(data)
+    # data = load_incident_rc_data(incident_rc_data_file)
+    # features, target = split_features_target(data)
+    features, target = load_incident_rc_data_splitted(incident_rc_data_file)
     categorized_target, reverse_map = categorize_target_np(target)
     categotical_target = build_target_categorical(categorized_target)
-    rescaled_features = rescale_input(features)
 
-    return rescaled_features, categotical_target, reverse_map
+    # rescaled_features = rescale_input(features)
+
+    return features, categotical_target, reverse_map
