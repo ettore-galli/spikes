9775181c69ae162b44e39eb02168fc18c4697ede
ettore-galli <ettore.galli@gmail.com>
2023-12-03 21:32:47 +0100

    score

--- deeplearning/openlearninglibrary/perceptron/perceptron_test.py
+++ deeplearning/openlearninglibrary/perceptron/perceptron_test.py
@@ -1,6 +1,7 @@
 import numpy as np
+from pytest import approx
 
-from perceptron.perceptron import classifier
+from perceptron.perceptron import classifier, score
 
 
 def test_classifier():
@@ -15,3 +16,17 @@ def test_classifier():
     expected = np.array([-1, 1, 1, -1])
 
     np.testing.assert_array_equal(results, expected)
+
+
+def test_score():
+    data = np.array([[1, 3], [1, 1], [5, 4], [6, 7]])
+    labels = np.array([1, 1, -1, -1])
+
+    th = np.array([1, -1])
+    th0 = 0.9
+
+    h1 = (th, th0)
+
+    result = score(h=h1, test_x=data, test_y=labels)
+    expected = 0.5
+    assert result == approx(expected)
