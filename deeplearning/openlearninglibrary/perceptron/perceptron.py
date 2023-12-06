36279f427e5211dc3abe8b307d74a41ce97c72b4
ettore-galli <ettore.galli@gmail.com>
2023-12-06 14:41:14 +0100

    Perceptron average

--- deeplearning/openlearninglibrary/perceptron/perceptron.py
+++ deeplearning/openlearninglibrary/perceptron/perceptron.py
@@ -37,3 +37,31 @@ def perceptron(
             break
 
     return theta.reshape((dimension, 1)), np.array([theta_0])
+
+
+def averaged_perceptron(
+    data: Data, labels: Labels, params: Params, hook: Optional[Hook] = None
+) -> Tuple[np.ndarray, np.ndarray]:
+    dimension = data.shape[0]
+    theta = np.zeros(dimension)
+    theta_0 = 0
+    theta_avg = np.zeros(dimension)
+    theta_0_avg = 0
+    number_of_runs = 0
+    for _ in range(params.get("T", 10)):
+        for sample, label in zip(data.T, labels.T):
+            result = np.dot(theta, sample) + theta_0
+            margin = label * result
+
+            if margin <= 0:
+                theta += label * sample
+                theta_0 += label
+                if hook:
+                    hook(theta, theta_0)
+            theta_avg += theta
+            theta_0_avg += theta_0
+            number_of_runs += 1
+
+    return (theta_avg / number_of_runs).reshape((dimension, 1)), np.array(
+        [theta_0_avg / number_of_runs]
+    )
--- deeplearning/openlearninglibrary/perceptron/perceptron_test.py
+++ deeplearning/openlearninglibrary/perceptron/perceptron_test.py
@@ -2,6 +2,7 @@ import numpy as np
 from pytest import approx
 
 from perceptron.perceptron import (
+    averaged_perceptron,
     perceptron,
     Data,
     Labels,
@@ -27,3 +28,16 @@ def test_perceptron():
 
     np.array_equal(classifier[0], np.array([-9.0, 18.0]))
     assert classifier[1] == approx(2.0)
+
+
+def test_averaged_perceptron():
+    data: Data = np.array([[2, 3, 9, 12], [5, 1, 6, 5]])
+    labels: Labels = np.array([[1, -1, 1, -1]])
+    params: Params = {"T": 100}
+    hook: Hook = plot_classifier
+    classifier = averaged_perceptron(data=data, labels=labels, params=params, hook=hook)
+
+    print([x.tolist() for x in classifier])
+
+    np.array_equal(classifier[0], np.array([-9.0, 18.0]))
+    assert classifier[1] == approx(1.9425)
