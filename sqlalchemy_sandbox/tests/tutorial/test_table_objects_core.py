37f62f1b680b382ad16825789095b68e9008134f
ettore-galli <ettore.galli@gmail.com>
2024-02-01 07:17:42 +0100

    objects core

--- sqlalchemy_sandbox/tests/tutorial/test_table_objects_core.py
+++ sqlalchemy_sandbox/tests/tutorial/test_table_objects_core.py
@@ -1,5 +1,10 @@
+from sqlalchemy import insert
+from tutorial.connection import create_db_engine
+from tutorial.connection_base import Connections
 from tutorial.table_objects_core import (
     phonebook,
+    phonebook_entry_type,
+    database_metadata,
 )
 
 
@@ -11,3 +16,12 @@ def test_phonebook_entry_type_properties():
 
 def test_phonebook_properties():
     assert [column.name for column in phonebook.primary_key.columns] == ["entry_id"]
+
+
+def test_creation():
+    engine = create_db_engine(Connections.SQLITE_IN_MEMORY.value)
+    database_metadata.create_all(bind=engine)
+    with engine.connect() as connection:
+        connection.execute(
+            insert(phonebook_entry_type).values([{"entry_type_id": 1, "type": "casa"}])
+        )
