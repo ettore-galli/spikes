20887b0db590e90d57ff05194784a691042ce6ac
ettore-galli <ettore.galli@gmail.com>
2024-02-02 20:53:28 +0100

    insert

---
+++ sqlalchemy_sandbox/tests/tutorial/test_core_operations.py
@@ -0,0 +1,19 @@
+from sqlalchemy import select
+
+from tutorial.orm_models import PhoneBookEntry
+from tutorial.core_operations import create_all_tables, insert_phone_book_entry
+from tutorial.connection_base import Connections
+from tutorial.connection import create_db_engine
+
+
+def test_insert_phone_book_entry():
+    engine = create_db_engine(Connections.SQLITE_IN_MEMORY.value)
+    create_all_tables(engine)
+    with engine.connect() as connection:
+        insert_phone_book_entry(connection=connection, name="Ettore", phone="123123123")
+        insert_phone_book_entry(
+            connection=connection, name="Ettore 2", phone="987987987"
+        )
+        records = connection.execute(select(PhoneBookEntry)).all()
+
+        assert records == [(1, "Ettore", "123123123"), (2, "Ettore 2", "987987987")]
