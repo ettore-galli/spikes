1ddeab354d05df75d66129dd19e79b7d956d782c
ettore-galli <ettore.galli@gmail.com>
2024-02-02 20:53:28 +0100

    insert

---
+++ sqlalchemy_sandbox/tutorial/core_operations.py
@@ -0,0 +1,23 @@
+"""
+Working with data
+
+https://docs.sqlalchemy.org/en/20/tutorial/data.html
+"""
+
+
+from typing import Optional
+
+from sqlalchemy import insert
+from sqlalchemy.engine.base import Connection, Engine
+
+from tutorial.orm_models import BaseModel, PhoneBookEntry
+
+
+def create_all_tables(engine: Engine):
+    BaseModel.metadata.create_all(bind=engine)
+
+
+def insert_phone_book_entry(
+    connection: Connection, name: Optional[str], phone: Optional[str]
+):
+    connection.execute(insert(PhoneBookEntry).values(name=name, phone=phone))
