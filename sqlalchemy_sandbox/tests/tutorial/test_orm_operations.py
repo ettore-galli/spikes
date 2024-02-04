a4ecd5a86dc5fad55a959ec6e0f97faa6060f0ab
ettore-galli <ettore.galli@gmail.com>
2024-02-04 10:59:39 +0100

    labels

--- sqlalchemy_sandbox/tests/tutorial/test_orm_operations.py
+++ sqlalchemy_sandbox/tests/tutorial/test_orm_operations.py
@@ -22,3 +22,38 @@ def test_insert_and_select():
         result = session.execute(select(PhoneBookEntry))
         record = [item.PhoneBookEntry for item in result.all()][0]
         assert record.asdict() == {"id": 1, "name": "Ettore", "phone": "123123123"}
+
+
+def test_use_scalars_select():
+    engine = create_db_engine(Connections.SQLITE_IN_MEMORY.value)
+
+    create_all_tables(engine)
+
+    with Session(engine) as session:
+        session.execute(insert(PhoneBookEntry).values(name="Ettore", phone="123123123"))
+
+        result = session.scalars(select(PhoneBookEntry))
+        data = [item.asdict() for item in result.all()]
+        assert data == [{"id": 1, "name": "Ettore", "phone": "123123123"}]
+
+
+def test_use_labels():
+    engine = create_db_engine(Connections.SQLITE_IN_MEMORY.value)
+
+    create_all_tables(engine)
+
+    with Session(engine) as session:
+        session.execute(insert(PhoneBookEntry).values(name="Ettore", phone="123123123"))
+
+        result = session.execute(
+            select(
+                PhoneBookEntry.name,
+                PhoneBookEntry.phone,
+                (PhoneBookEntry.name + "/" + PhoneBookEntry.phone).label("short"),
+            )
+        )
+        data = [item._asdict() for item in result.all()]
+
+        assert data == [
+            {"name": "Ettore", "phone": "123123123", "short": "Ettore/123123123"}
+        ]
--- sqlalchemy_sandbox/tutorial/orm_base.py
+++ sqlalchemy_sandbox/tutorial/orm_base.py
@@ -1,8 +1,14 @@
 from typing import Dict
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Row
+
+
+def row_to_dict(row: Row) -> Dict:
+    return row._asdict()
 
 
 # pylint: disable=too-few-public-methods
 class BaseModel(DeclarativeBase):
     def asdict(self) -> Dict:
+        """https://stackoverflow.com/a/11884806/5538793"""
         return {c.name: getattr(self, c.name, None) for c in self.__table__.columns}
