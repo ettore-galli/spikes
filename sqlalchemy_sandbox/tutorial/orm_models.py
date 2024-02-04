1956dba21c20c9beb86bf2307960ae573fc8d21c
ettore-galli <ettore.galli@gmail.com>
2024-02-04 14:37:21 +0100

    dataset

--- sqlalchemy_sandbox/tutorial/orm_models.py
+++ sqlalchemy_sandbox/tutorial/orm_models.py
@@ -5,9 +5,18 @@ from tutorial.orm_base import BaseModel
 
 
 # pylint: disable=too-few-public-methods
+class EntryType(BaseModel):
+    __tablename__ = "entry_type"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    description: Mapped[str] = mapped_column(String(30), nullable=False)
+
+
+# pylint: disable=too-few-public-methods
 class PhoneBookEntry(BaseModel):
     __tablename__ = "phone_book_entry"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(100), nullable=True)
     phone: Mapped[str] = mapped_column(String(30), nullable=True)
+    entry_type_id: Mapped[int] = mapped_column(Integer, nullable=True)
