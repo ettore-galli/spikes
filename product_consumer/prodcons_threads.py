e9ec1c6a69de7b4d72b0a5917012ac8668849240
ettore-galli <ettore.galli@gmail.com>
2024-04-01 23:01:14 +0200

    Improve example

--- product_consumer/prodcons_threads.py
+++ product_consumer/prodcons_threads.py
@@ -1,4 +1,6 @@
 import csv
+import time
+import random
 from dataclasses import dataclass
 from enum import Enum
 from queue import Queue
@@ -21,12 +23,16 @@ class ThreadData:
 def prodcons():
     QSIZE = 8
     WORKERS = 5
-
+    DELAY_BASE = 2
     producers_queue = Queue(QSIZE)
     consumers_queue = Queue(QSIZE)
 
+    def delayrand():
+        time.sleep(DELAY_BASE * random.random())
+
     def producer(top: int):
         for item in read_input("input/input.csv"):
+            delayrand()
             producers_queue.put(ThreadData(payload=item))
         for _ in range(WORKERS):
             producers_queue.put(ThreadData(data_type=ThreadDataType.TERMINATE))
@@ -41,24 +47,29 @@ def prodcons():
             result = f"[PROCESSED: {data_item.payload}]"
 
             result_item = ThreadData(payload=result)
+            delayrand()
             consumers_queue.put(result_item)
             producers_queue.task_done()
 
-    def consumer():
+    def threads_are_alive(threads: Dict[int, Thread]) -> bool:
+        return any(tread.is_alive() for tread in threads.values())
+
+    def consumer(workers: Dict[int, Thread]):
         while True:
             data_item: ThreadData = consumers_queue.get()
             if data_item.data_type == ThreadDataType.TERMINATE:
-                print("Terminate consumer")
-                break
+                if not threads_are_alive(workers):
+                    print("Terminate consumer")
+                    break
             result = f"[CONSUMED: {data_item.payload}]"
             print(result)
             consumers_queue.task_done()
 
-    thread_consumer = Thread(name="consumer", target=consumer)
+    thread_work: Dict[int, Thread] = {}
+
+    thread_consumer = Thread(name="consumer", target=consumer, args=(thread_work,))
     thread_consumer.daemon = False
     thread_consumer.start()
-
-    thread_work: Dict[int, Thread] = {}
 
     for nwork in range(WORKERS):
         thread_work[nwork] = Thread(name="worker", target=worker)
