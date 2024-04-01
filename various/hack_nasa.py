aa02f9a4d1c370eb97af155f9dc893cbe1979c65
ettore-galli <ettore.galli@gmail.com>
2024-04-01 21:49:19 +0200

    hack nasa 1 april fool

---
+++ various/hack_nasa.py
@@ -0,0 +1,8 @@
+import time
+
+if __name__ == "__main__":
+    print("Hacking NASA...")
+    for percent in [10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 98, 99, 99.5, 99.7, 99.9]:
+        time.sleep(1)
+        print(f"Hacking progress: {percent}%...")
+    print("Hacking NASA completed!")
