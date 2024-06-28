73dae61f070897ebf96fc54cd4ff5175d88f903f
ettore-galli <ettore.galli@gmail.com>
2024-06-28 20:48:50 +0200

    di choice example

--- depencency_injection/dependency.py
+++ depencency_injection/dependency.py
@@ -1,8 +1,13 @@
 from depencency_injection.reader.reader_base import BaseFileReader
+from depencency_injection.reader.fake_reader import FakeDataFileReader
+
+from depencency_injection.di.di import dependency
 from depencency_injection.reader.file_reader import DataFileReader
 
-from depencency_injection.di.di import DependencyInjectionManager
+
+def make_normal_dependency():
+    dependency.add_config(BaseFileReader, DataFileReader)
 
 
-dependency = DependencyInjectionManager()
-dependency.add_config(BaseFileReader, DataFileReader)
+def make_fake_dependency():
+    dependency.add_config(BaseFileReader, FakeDataFileReader)
--- depencency_injection/main_fake.py
+++ depencency_injection/main_fake.py
@@ -1,9 +1,10 @@
-from depencency_injection.di.di import dependency
-from depencency_injection.dependency import make_normal_dependency
+from depencency_injection.dependency import make_fake_dependency
 from depencency_injection.process import display_file_content
 from depencency_injection.reader.reader_base import BaseFileReader
 
-make_normal_dependency()
+from depencency_injection.di.di import dependency
+
+make_fake_dependency()
 
 if __name__ == "__main__":
     display_file_content(
