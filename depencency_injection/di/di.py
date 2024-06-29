ceeb4531f9baaa729c77e47f0e6687ff15b307d8
ettore-galli <ettore.galli@gmail.com>
2024-06-29 09:52:38 +0200

    working di

--- depencency_injection/di/di.py
+++ depencency_injection/di/di.py
@@ -1,6 +1,7 @@
 from abc import ABC
 from dataclasses import dataclass, field
-from typing import Dict, Type
+from functools import wraps
+from typing import Callable, Dict, List, Type
 
 
 @dataclass
@@ -12,6 +13,27 @@ class DependencyInjectionManager:
 
     def resolve(self, interface: Type[ABC]) -> Type:
         return self.di_map[interface]
+
+    def can_resolve(self, interface: Type[ABC]) -> bool:
+        return interface in self.di_map
+
+    def retrieve_injectable_parameters(self, injectand: Callable) -> List:
+        return list(injectand.__annotations__.items())
+
+    def inject_dependencies(self, injectand: Callable) -> Callable:
+        @wraps(injectand)
+        def injected_function(*args, **kwargs):
+            injected_parameters: Dict = {
+                parameter_name: dependency.resolve(parameter_type)
+                for parameter_name, parameter_type in self.retrieve_injectable_parameters(
+                    injectand=injectand
+                )
+                if dependency.can_resolve(parameter_type)
+            }
+            residual_kwargs = {arg:value for arg, value in kwargs.items() if arg not in injected_parameters}
+            return injectand(*args, **{**residual_kwargs, **injected_parameters})
+
+        return injected_function
 
 
 dependency = DependencyInjectionManager()
--- depencency_injection/process.py
+++ depencency_injection/process.py
@@ -1,7 +1,8 @@
-from typing import Type
+from depencency_injection.di.di import dependency
 from depencency_injection.reader.reader_base import BaseFileReader
 
 
-def display_file_content(file_reader: Type[BaseFileReader], file: str) -> None:
-    for index, row in enumerate(file_reader().read_data(file=file)):
+@dependency.inject_dependencies
+def display_file_content(file_reader: BaseFileReader, file: str) -> None:
+    for index, row in enumerate(file_reader.read_data(file=file)):
         print(f"{index}: {row}")
