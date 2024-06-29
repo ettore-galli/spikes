730d9c69e5e1e59ecd1c2c22f004eab4d1608436
ettore-galli <ettore.galli@gmail.com>
2024-06-29 17:31:36 +0200

    Injection works via call

--- dependency-injection/dependency_injection/process.py
+++ dependency-injection/dependency_injection/process.py
@@ -2,7 +2,9 @@ from dependency_injection.di.di import dependency
 from dependency_injection.reader.reader_base import BaseFileReader
 
 
-@dependency.inject_dependencies
 def display_file_content(file_reader: BaseFileReader, file: str) -> None:
     for index, row in enumerate(file_reader.read_data(file=file)):
         print(f"{index}: {row}")
+
+
+display_file_content_process = dependency.inject_dependencies(display_file_content)
