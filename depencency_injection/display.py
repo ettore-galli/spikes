d1f6c3d416581f778d5af4ff3bc48d3dc659d911
ettore-galli <ettore.galli@gmail.com>
2024-06-29 17:34:54 +0200

    Injection works via call

--- depencency_injection/display.py
+++
@@ -1,9 +0,0 @@
-from depencency_injection.process import display_file_content
-
-# from depencency_injection.di.di import dependency
-# from depencency_injection.reader.reader_base import BaseFileReader
- 
-
-def display(file: str):
-    # display_file_content(file_reader=dependency.resolve(BaseFileReader), file=file)
-    display_file_content(file=file)
--- depencency_injection/process.py
+++
@@ -1,8 +0,0 @@
-from depencency_injection.di.di import dependency
-from depencency_injection.reader.reader_base import BaseFileReader
-
-
-@dependency.inject_dependencies
-def display_file_content(file_reader: BaseFileReader, file: str) -> None:
-    for index, row in enumerate(file_reader.read_data(file=file)):
-        print(f"{index}: {row}")
