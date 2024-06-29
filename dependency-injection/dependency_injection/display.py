8bb82acbb5cb401f0f20fc917dc5fdde262df392
ettore-galli <ettore.galli@gmail.com>
2024-06-29 17:31:36 +0200

    Injection works via call

--- dependency-injection/dependency_injection/display.py
+++ dependency-injection/dependency_injection/display.py
@@ -1,5 +1,5 @@
-from dependency_injection.process import display_file_content
+from dependency_injection.process import display_file_content_process
 
 
 def display(file: str):
-    display_file_content(file=file)
+    display_file_content_process(file=file)
--- dependency-injection/dependency_injection/main.py
+++ dependency-injection/dependency_injection/main.py
@@ -5,4 +5,4 @@ from dependency_injection.display import display
 make_normal_dependency()
 
 if __name__ == "__main__":
-    display(file="dependency_injection/data/sample.txt")
+    display(file="./dependency_injection/data/sample.txt")
