58525d5f07a42d83478b5f93ef16fc82af0a710c
Ettore Galli <ettore.galli@eng.it>
2024-09-17 21:55:14 +0200

    basic multiworker

--- multi_processing/main.py
+++ multi_processing/main.py
@@ -1,7 +1,65 @@
 from datetime import datetime
+
+from multiprocessing import Queue
+import multiprocessing
+from time import sleep
+
 from multi_processing.common.app_logger import make_logger
 from multi_processing.common.connection import create_session_maker
 from multi_processing.repo.data_repo import DataRepo
+
+
+POISON_PILL: str = "*END"
+
+
+def worker(database_url: str, data_q: Queue):
+    session_maker = create_session_maker(database=database_url)
+    with session_maker() as session:
+        while True:
+            data = data_q.get()
+            if data == POISON_PILL:
+                break
+            repo: DataRepo = DataRepo(session=session)
+            sleep(0.3)
+            result = f"processing data: {data} {datetime.now().isoformat()}"
+            # logger.info("adding data: %s", data)
+            print(f"processing {data} ...")
+            repo.add_data(result)
+
+
+def process_main(database_url: str, log_file: str):
+    data_q: Queue = Queue()
+    workers = 3
+
+    multiprocessing_context = multiprocessing.get_context("spawn")
+
+    processes = [
+        multiprocessing_context.Process(
+            target=worker,
+            args=(
+                database_url,
+                data_q,
+            ),
+        )
+        for _ in range(workers)
+    ]
+
+    for process in processes:
+        process.start()
+
+    logger = make_logger(log_file=log_file, name=__name__)
+
+    input_data = [f"item [{index}]" for index in range(100)]
+
+    for item in input_data:
+        data_q.put(item)
+
+    for _ in range(workers):
+        data_q.put(POISON_PILL)
+    data_q.close()
+
+    for process in processes:
+        process.join()
 
 
 if __name__ == "__main__":
@@ -10,12 +68,5 @@ if __name__ == "__main__":
     )
 
     log_file: str = "./logs/app.log"
-    logger = make_logger(log_file=log_file, name=__name__)
 
-    session_maker = create_session_maker(database=database_url)
-
-    with session_maker() as session:
-        repo: DataRepo = DataRepo(session=session)
-        data = f"add data demo {datetime.now().isoformat()}"
-        logger.info("adding data: %s", data)
-        repo.add_data(f"add data demo {datetime.now().isoformat()}")
+    process_main(database_url=database_url, log_file=log_file)
