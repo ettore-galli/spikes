0e586ca3357caf615cd017dea0c338d02bba1579
ettore-galli <ettore.galli@gmail.com>
2024-12-26 11:05:06 +0100

    Basic probability

---
+++ statistics/foundations-1/test_probability.py
@@ -0,0 +1,45 @@
+from probability import (
+    combinazioni,
+    combine_powers,
+    factorial_range,
+    permutazioni,
+    permutazioni_x_n,
+)
+
+
+def test_permutazioni():
+    assert permutazioni(5) == 120
+    assert permutazioni_x_n(3, 8) == 336
+
+
+def test_combinazioni():
+    assert combinazioni(12, 4) == 495
+    assert combinazioni(10, 2) == 45
+    assert combinazioni(5, 2) == 10
+
+
+def test_factorial_range():
+    assert factorial_range(7) == {
+        1: 1,
+        2: 1,
+        3: 1,
+        4: 1,
+        5: 1,
+        6: 1,
+        7: 1,
+    }
+
+
+def test_combine_powers():
+    assert combine_powers([{1: 1, 2: 3, 3: 2, 4: 1}, {2: 3, 4: 3}]) == {
+        1: 1,
+        2: 6,
+        3: 2,
+        4: 4,
+    }
+    assert combine_powers([{1: 1, 2: 3, 3: 2, 4: 1}, {2: 3, 4: 3}, {2: 1, 3: 1}]) == {
+        1: 1,
+        2: 7,
+        3: 3,
+        4: 4,
+    }
