47659a7198011d34e0ff614e9099007fac110f00
Ettore Galli <ettore.galli@eng.it>
2025-02-12 23:17:32 +0100

    split macro passes

--- macromicro/test_macromicro.py
+++ macromicro/test_macromicro.py
@@ -1,8 +1,19 @@
-from macromicro.macromicro import render_splitted_lists, expected, data, threshold
+from macromicro.macromicro import (
+    render_splitted_lists,
+    expected,
+    data,
+    split_macros,
+    threshold,
+)
 
 
 def test_render_splitted_lists():
     allresult = list(
         render_splitted_lists(data_macro=data["macro"], threshold=threshold)
     )
+    assert allresult == expected
+
+
+def test_split_macros():
+    allresult = list(split_macros(data_macro=data["macro"], threshold=threshold))
     assert allresult == expected
