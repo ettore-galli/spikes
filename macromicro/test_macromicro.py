6660e95677d15be67ac4692a994a643a9907325d
Ettore Galli <ettore.galli@eng.it>
2025-02-12 21:56:17 +0100

    rename

--- macromicro/test_macromicro.py
+++ macromicro/test_macromicro.py
@@ -1,6 +1,6 @@
-from macromicro.macromicro import render_splitted, expected, data, threshold
+from macromicro.macromicro import render_splitted_macros, expected, data, threshold
 
 
-def test_render_splitted():
-    allresult = list(render_splitted(data_macro=data["macro"], threshold=threshold))
+def test_render_splitted_macros():
+    allresult = list(render_splitted_macros(data_macro=data["macro"], threshold=threshold))
     assert [allresult] == expected
