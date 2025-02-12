f70ed53b78810a7bfa8b17c750ab6a0c6f36f2d8
Ettore Galli <ettore.galli@eng.it>
2025-02-12 22:12:41 +0100

    PASS

--- macromicro/test_macromicro.py
+++ macromicro/test_macromicro.py
@@ -1,6 +1,8 @@
-from macromicro.macromicro import render_splitted_macros, expected, data, threshold
+from macromicro.macromicro import render_splitted_lists, expected, data, threshold
 
 
-def test_render_splitted_macros():
-    allresult = list(render_splitted_macros(data_macro=data["macro"], threshold=threshold))
-    assert [allresult] == expected
+def test_render_splitted_lists():
+    allresult = list(
+        render_splitted_lists(data_macro=data["macro"], threshold=threshold)
+    )
+    assert allresult == expected
