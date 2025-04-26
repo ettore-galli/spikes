25eabb4f2759cc893c086cd307de28ea7705a630
ettore-galli <ettore.galli@gmail.com>
2025-04-26 15:08:10 +0200

    refactor

--- python-parallel/challenges/msort/msort.py
+++ python-parallel/challenges/msort/msort.py
@@ -4,28 +4,35 @@ from typing import List, Union
 ListElement = Union[float, int]
 
 
+def do_merge_sort_merge(
+    left: List[ListElement], right: List[ListElement]
+) -> List[ListElement]:
+    merged = []
+
+    i_left = 0
+    i_right = 0
+
+    while i_left < len(left) and i_right < len(right):
+        if left[i_left] <= right[i_right]:
+            merged.append(left[i_left])
+            i_left += 1
+        else:
+            merged.append(right[i_right])
+            i_right += 1
+
+    merged.extend(left[i_left:])
+    merged.extend(right[i_right:])
+    return merged
+
+
 def do_merge_sort(array: List[ListElement]) -> List[ListElement]:
     if len(array) > 1:
         mid = len(array) // 2
         left = do_merge_sort(array[:mid])
         right = do_merge_sort(array[mid:])
-        result = []
 
-        i_left = 0
-        i_right = 0
+        return do_merge_sort_merge(left=left, right=right)
 
-        while i_left < len(left) and i_right < len(right):
-            if left[i_left] <= right[i_right]:
-                result.append(left[i_left])
-                i_left += 1
-            else:
-                result.append(right[i_right])
-                i_right += 1
-
-        result.extend(left[i_left:])
-        result.extend(right[i_right:])
-
-        return result
     else:
         return array
 
