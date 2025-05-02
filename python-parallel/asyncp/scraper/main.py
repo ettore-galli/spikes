ad712a64fdd49a986c28df4dc8bfbe3f03032a5f
ettore-galli <ettore.galli@gmail.com>
2025-05-02 10:13:09 +0200

    Gather

--- python-parallel/asyncp/scraper/main.py
+++ python-parallel/asyncp/scraper/main.py
@@ -3,9 +3,10 @@ import sys
 
 from aiohttp import ClientSession
 from asyncp.scraper.file_reader import read_url_list
-from asyncp.scraper.downloader import download_url, look_for_links
+
 from asyncp.scraper.file_writer import save_url_list
 from asyncp.scraper.notifier import Notifier
+from asyncp.scraper.scraper import perform_single_site_scraping
 
 """
 python asyncp/scraper/main.py example-data/urls.txt output/output.txt
@@ -15,27 +16,32 @@ python asyncp/scraper/main.py example-data/urls.txt output/output.txt
 async def process_single_site(
     session: ClientSession, output_file: str, url: str, notifier: Notifier
 ) -> None:
-    result = await download_url(session, url)
-    if result.success:
-        if result.content:
-            await notifier.notify(message=f"Retrieving links for {url}")
-            await save_url_list(
-                output_file=output_file, url_list=look_for_links(result.content)
-            )
-            await notifier.notify(message=f"Links for {url} saved to {output_file}")
-        else:
-            await notifier.notify(message=f"Failed to download {url}")
+    await notifier.notify(message=f"Scraping {url}")
+    scraping_result = await perform_single_site_scraping(
+        client_session=session,
+        url=url,
+    )
+
+    if scraping_result.success:
+        await notifier.notify(message=f"Scraping success for {url}")
+        await save_url_list(output_file=output_file, url_list=scraping_result.links)
+        await notifier.notify(message=f"Links for {url} saved to {output_file}")
+    else:
+        await notifier.notify(message=f"Failed to download {url}")
 
 
 async def main(url_list_file: str, output_file: str):
     async with ClientSession() as session:
-        for url in await read_url_list(url_list_file):
-            await process_single_site(
+        processes = [
+            process_single_site(
                 session=session,
                 output_file=output_file,
                 url=url,
                 notifier=Notifier(),
             )
+            for url in await read_url_list(url_list_file)
+        ]
+        await asyncio.gather(*processes)
 
 
 if __name__ == "__main__":
