25e0a019a66b8c4c37c95b067ff0e00ba783eb9e
ettore-galli <ettore.galli@gmail.com>
2025-06-17 21:56:26 +0200

    dicotomic

--- various/test_dicotomic.py
+++ various/test_dicotomic.py
@@ -1,11 +1,22 @@
-from various.dicotomic import solve_dicotomic_monotonically_increasing
+from various.dicotomic import (
+    solve_dicotomic_monotonically_decreasing,
+    solve_dicotomic_monotonically_increasing,
+)
 
 
 def test_solve_dicotomic_monotonically_increasing():
-    delta = 0.0001
     expected_x = 2
     x = solve_dicotomic_monotonically_increasing(
-        function=lambda x: x**3, target=8, x_domain=(-1000, 1000), delta=delta
+        function=lambda x: x**3, target=8, x_domain=(-1000, 1000)
     )
 
-    assert abs(x - expected_x) < delta
+    assert abs(x - expected_x) < 0.0001
+
+
+def test_solve_dicotomic_monotonically_decreasing():
+    expected_x = 4
+    x = solve_dicotomic_monotonically_decreasing(
+        function=lambda x: 1 / x, target=0.25, x_domain=(0.000001, 1000)
+    )
+
+    assert abs(x - expected_x) < 0.0001
