d36f3fb4d4ceca254da0bc1deafc7bc323b6377d
ettore-galli <ettore.galli@gmail.com>
2025-08-01 08:57:34 +0200

    Not working but maybe i understand something

--- lisp/start-swank.lisp
+++ lisp/start-swank.lisp
@@ -1,18 +1,16 @@
 (ql:quickload :swank)
 
-;; Configurazioni ESSENZIALI per Docker
-(setq swank:*communication-style* :fd-handler)   
-
-;; Logging
-(let ((log-file #P"/tmp/swank.log"))
-  (ensure-directories-exist log-file)
-  (setf swank::*log-events* t
-        swank::*log-output* (open log-file :direction :output 
-                                      :if-exists :append 
-                                      :if-does-not-exist :create)))
+(setq 
+    swank:*communication-style* :fd-handler
+    swank::*log-events* t
+)
+ 
 
 (swank:create-server 
   :port 4005
   :style :fd-handler   
   :dont-close t
-  :interface "0.0.0.0")   
+  :interface "0.0.0.0"
+)   
+
+(force-output)
